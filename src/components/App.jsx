6fd2a2e485193e9b212adb805f20f1eb6dee12c5
ElenaLukashevych <elukashevych@gmail.com>
2022-05-29 20:20:15 +0300

    dinosaur

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import dinosaur from '../data/dinosaur'
+import Lenka from './Lenka'
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Lenka dinosaur={dinosaur}/>
     </div>
   );
 };
