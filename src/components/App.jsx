b755e9725491e3aaeabbdaf4ba67a7999eee5a7b
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-05-29 20:39:12 +0300

    add Ihor files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import Car from './Ihor';
+import car from '../data/car.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Car images={car}/>
     </div>
   );
 };
