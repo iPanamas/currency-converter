6cf702cecc1876f86843ebf4e45421ad85b94d04
@s_zhsn <s.zhosan@gmail.com>
2022-06-12 19:56:50 +0300

    code-edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import Fox from './Vladimir';
+/* import Fox from './Vladimir';
 import foxData from '../data/fox.json';
 
 import Anna from './Anna';
@@ -17,20 +17,51 @@ import Lenka from './Lenka';
 import dinosaur from '../data/dinosaur';
 
 import Natalia from './Natalia';
-import flowers from '../data/flower.json';
+import flowers from '../data/flower.json'; */
+import { Component } from 'react';
+import Converter from './Converter';
+import fetchUserCurrency from 'service/currencyAPI';
+import currenciesList from '../data/currenciesList.json';
 
-export const App = () => {
-  return (
-    <div>
-      <Fox stats={foxData} />
-      <Anna anime={anime} />
-      <Car images={car} />
-      <Lenka dinosaur={dinosaur} />
-      <Natalia flowers={flowers} />
-      <Sunrises sunrises={sunrises} />
-      <Plane plane={plane} />
-    </div>
-  );
-};
+export class App extends Component {
+  state = { userCurrency: 'USD' };
+  componentDidMount() {
+    if (window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(position => {
+        const lat = position?.coords?.latitude;
+        const lon = position?.coords?.longitude;
+        fetchUserCurrency(lat, lon).then(result =>
+          this.setState({ userCurrency: result })
+        );
+      });
+    }
+  }
+
+  handleChange = event => {
+    this.setState({ userCurrency: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <select
+          onChange={this.handleChange}
+          value={this.state.userCurrency}
+          name="currencyList"
+          id=""
+        >
+          {Object.keys(currenciesList).map(currency => {
+            return (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            );
+          })}
+        </select>
+        <p>{this.state.userCurrency}</p>
+      </div>
+    );
+  }
+}
 
 export default App;
