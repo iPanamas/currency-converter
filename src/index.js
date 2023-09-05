e13963a6421a8084d8c12b0b74bdc770608e1097
Igor Panama <88848737+iPanamas@users.noreply.github.com>
2023-09-05 12:13:07 +0300

    change route basename

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { theme } from 'constants/theme';
-import App from 'components/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { theme } from "constants/theme";
+import App from "components/App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/group3/">
+      <BrowserRouter basename="/currency-converter/">
         <App />
       </BrowserRouter>
     </ThemeProvider>
